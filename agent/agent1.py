2eb38850d327954965563e8bae26b0bedb3aa031
frUTCS16 <farzan.memarian@utexas.edu>
2018-12-01 18:51:33 -0600

    scenario 1 implemented, objects removed after collecting, only FC layers

--- agent/agent1.py
+++ agent/agent1.py
@@ -7,7 +7,7 @@ from keras.layers import Dense, Activation
 from keras.optimizers import SGD, RMSprop
 import sys
 sys.path.append('../')
-from envs.gridworld_relational import Gridworld
+from envs.gridworld_relational1 import Gridworld
 from pdb import set_trace
 
 # DEFAULT ARCHITECTURE FOR THE META CONTROLLER
@@ -129,36 +129,37 @@ class hDQN:
         return controller
 
     def select_goal(self, agent_state):
-        objects = env.curre
-        agent_env_state = np.concatenate((self.env.grid_flattened, agent_state), axis=1)
         if self.meta_epsilon < random.random():
-            pred = self.meta_controller.predict(agent_env_state, verbose=0)
-            # print("pred shape: " + str(pred.shape))
-            goal_idx = np.argmax(pred)
-            goal = self.env.original_objects[goal_idx]
+            Q = []
+            for goal in self.env.current_objects:
+                state_goal = np.concatenate((self.env.grid_flat, agent_state, 
+                                             np.array(goal).reshape((1,1))), axis=1)
+            
+                pred = self.meta_controller.predict(state_goal, verbose=0)
+                Q.append(pred)
+            goal_idx = np.argmax(Q)
+            goal = self.env.current_objects[goal_idx]
         else:
             print("Exploring")
-            goal_idx, goal = self.random_goal_selection(agent_state)
+            goal = self.random_goal_selection()
+
+        # update environment
         self.env.selected_goals.append(goal)
         self.env.update_target_goal()
-        return goal_idx, goal
+        return goal
 
-    def random_goal_selection(self, agent_state):
+    def random_goal_selection(self):
         # Don't call this function directly, it would always be called from select_goal()
-        current_object_occupied = self.env.grid_mat[agent_state[0,0], agent_state[0,1]]
-        done = False
-        while not done:
-            goal_idx = np.random.choice(len(self.env.original_objects))
-            goal = self.env.original_objects[goal_idx]
-            if goal != current_object_occupied:
-                done = True
-        return goal_idx, goal
+        goal_idx = np.random.choice(len(self.env.current_objects))
+        goal = self.env.original_objects[goal_idx]
+        return goal
 
-    def select_action(self, agent_state, goal, goal_idx):
-        agent_env_state = np.concatenate((self.env.grid_flattened, agent_state), axis=1)
+
+    def select_action(self, agent_state, goal):
+        agent_env_state = np.concatenate((self.env.grid_flat, agent_state), axis=1)
         state_goal_feature = np.concatenate((agent_env_state, np.array(goal).reshape((1,1))), axis=1)
         if random.random() > self.epsilon:
-            print("controller selected action")
+            # print("controller selected action")
             # ensures that only actions that cause movement are chosen
             action_probs = self.controller.predict(state_goal_feature, verbose=0)        
             allowable_action_idxs = self.env.allowable_action_idxs[self.env.state[0,0], self.env.state[0,1]]
@@ -177,7 +178,7 @@ class hDQN:
         return action_idx, action
 
     def random_action_selection(self):
-        print("random action selected")
+        # print("random action selected")
         allowable_action_idxs = self.env.allowable_action_idxs[self.env.state[0,0], self.env.state[0,1]]
         action_idx = np.random.choice(allowable_action_idxs)
         action = self.env.all_actions[action_idx]
@@ -187,38 +188,63 @@ class hDQN:
         if meta:
             self.meta_memory.append(experience)
             if len(self.meta_memory) > 1000000:
-                self.meta_memory = self.meta_memory[-100:]
+                self.meta_memory = self.meta_memory[-1000:]
         else:
             self.memory.append(experience)
             if len(self.memory) > 1000000:
                 self.memory = self.memory[-1000000:]
 
+    def Q_controller(self, state_vectors, target):
+        if target:
+            try:
+                Q_preds = self.target_controller.predict(state_vectors, verbose=0)
+            except Exception as e:
+                state_vectors = np.expand_dims(state_vectors, axis=0)
+                Q_preds = self.target_controller.predict(state_vectors, verbose=0)
+            return Q_preds, state_vectors
+        else:
+            try:
+                Q_preds = self.controller.predict(state_vectors, verbose=0)
+            except Exception as e:
+                state_vectors = np.expand_dims(state_vectors, axis=0)
+                Q_preds = self.controller.predict(state_vectors, verbose=0)
+            return Q_preds, state_vectors
+
+    def Q_meta(self, state_vectors, target):
+        if target:
+            try:
+                Q_preds = self.target_meta_controller.predict(state_vectors, verbose=0)
+            except Exception as e:
+                state_vectors = np.expand_dims(state_vectors, axis=0)
+                Q_preds = self.target_meta_controller.predict(state_vectors, verbose=0)
+            return Q_preds, state_vectors
+        else:
+            try:
+                Q_preds = self.meta_controller.predict(state_vectors, verbose=0)
+            except Exception as e:
+                state_vectors = np.expand_dims(state_vectors, axis=0)
+                Q_preds = self.meta_controller.predict(state_vectors, verbose=0)
+            return Q_preds, state_vectors
+
     def _update_controller(self):
         sample_size = min(self.batch_size, len(self.memory))
         exps = [random.choice(self.memory) for _ in range(sample_size)]
-        state_vectors = np.squeeze(np.asarray([np.concatenate([self.env.grid_flattened, 
-                        exp.agent_state, exp.goal.reshape((1,1))], axis=1) for exp in exps]))
-        next_state_vectors = np.squeeze(np.asarray([np.concatenate([self.env.grid_flattened, 
-                        exp.next_agent_state, exp.goal.reshape((1,1))], axis=1) for exp in exps]))
-        try:
-            Q_preds = self.controller.predict(state_vectors, verbose=0)
-        except Exception as e:
-            state_vectors = np.expand_dims(state_vectors, axis=0)
-            Q_preds = self.controller.predict(state_vectors, verbose=0)
-        
-        try:
-            next_state_Q_preds = self.target_controller.predict(next_state_vectors, verbose=0)
-        except Exception as e:
-            next_state_vectors = np.expand_dims(next_state_vectors, axis=0)
-            next_state_Q_preds = self.target_controller.predict(next_state_vectors, verbose=0)
-        
+        state_vectors = np.squeeze(np.asarray([np.concatenate([exp.state, 
+                        exp.goal], axis=1) for exp in exps]))
+        next_state_vectors = np.squeeze(np.asarray([np.concatenate([exp.next_state, 
+                        exp.goal], axis=1) for exp in exps]))
+
+        Q_targets, state_vectors  = self.Q_controller(state_vectors, target=False)
+        next_state_Q_preds, next_state_vectors = self.Q_controller(next_state_vectors, target=True)
+
         for i, exp in enumerate(exps):
-            Q_preds[i,exp.action] = exp.reward
-            if not exp.done:
-                Q_preds[i,exp.action] += self.gamma * max(next_state_Q_preds[i])
+            Q_targets[i,exp.action] = exp.reward
+            # if not exp.controller_done:
+            Q_targets[i,exp.action] += self.gamma * max(next_state_Q_preds[i])
         # Q_preds = np.asarray(Q_preds)
-        self.controller.fit(state_vectors, Q_preds, verbose=0)
+        self.controller.fit(state_vectors, Q_targets, verbose=0)
         
+
         #Update target network
         controller_weights = self.controller.get_weights()
         controller_target_weights = self.target_controller.get_weights()
@@ -230,27 +256,23 @@ class hDQN:
         if 0 < len(self.meta_memory):
             sample_size = min(self.meta_batch_size, len(self.meta_memory))
             exps = [random.choice(self.meta_memory) for _ in range(sample_size)]
-            state_vectors = np.squeeze(np.asarray([np.concatenate([self.env.grid_flattened, 
-                          exp.agent_state], axis=1) for exp in exps]))
-            next_state_vectors = np.squeeze(np.asarray([np.concatenate([self.env.grid_flattened, 
-                               exp.next_agent_state], axis=1) for exp in exps]))
-            try:
-                Q_preds = self.meta_controller.predict(state_vectors, verbose=0)
-            except Exception as e:
-                state_vectors = np.expand_dims(state_vectors, axis=0)
-                Q_preds = self.meta_controller.predict(state_vectors, verbose=0)
-            
-            try:
-                next_state_Q_preds = self.target_meta_controller.predict(next_state_vectors, verbose=0)
-            except Exception as e:
-                next_state_vectors = np.expand_dims(next_state_vectors, axis=0)
-                next_state_Q_preds = self.target_meta_controller.predict(next_state_vectors, verbose=0)
-            
+            state_vectors = np.squeeze(np.asarray([np.concatenate([exp.state, exp.goal], 
+                                    axis=1) for exp in exps]))
+
+            Q_preds, state_vectors = self.Q_meta(state_vectors, target=False)
+            Q_targets = np.zeros_like(Q_preds, dtype=int)
             for i, exp in enumerate(exps):
-                Q_preds[i,np.argmax(exp.goal)] = exp.reward
-                if not exp.done:
-                    Q_preds[i,np.argmax(exp.goal)] += self.gamma * max(next_state_Q_preds[i])
-            self.meta_controller.fit(state_vectors, Q_preds, verbose=0)
+                Q_targets[i,0] = exp.reward
+
+                # if not exp.done:
+                if exp.next_available_goals:
+                    # this block finds the max Q in the next state for this particular experiment
+                    next_state_vectors = np.squeeze(np.asarray([np.concatenate([exp.state, next_goal.reshape((1,1))], 
+                                    axis=1) for next_goal in exp.next_available_goals]))
+                    next_state_Q_preds, next_state_vectors = self.Q_meta(next_state_vectors, target=True)
+                    Q_targets[i,0] += self.gamma * max(next_state_Q_preds)
+
+            self.meta_controller.fit(state_vectors, Q_targets, verbose=0)
             
             #Update target network
             meta_weights = self.meta_controller.get_weights()
--- run1.py
+++ run1.py
@@ -4,18 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import namedtuple
 from envs.gridworld_relational1 import Gridworld
-from agent.agent import hDQN
+from agent.agent1 import hDQN
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.optimizers import SGD, RMSprop
 from pdb import set_trace 
+import pickle
 
 plt.style.use('ggplot')
 
 def init():
     # TRAIN HRL PARAMS
     num_thousands = 1
-    num_epis = 10000
+    num_epis = 100
 
 
     # GRID WORLD GEOMETRICAL PARAMETERS
@@ -29,15 +30,15 @@ def init():
 
     # EXTRINSIC REWARDS
     not_moving_reward = -1
-    game_over_reward = -100
+    game_over_reward = -1000
     step_reward = -1
-    current_goal_reward = 500
+    current_goal_reward = 300
     final_goal_reward = 10000
 
     # INTRINSIC REWARDS
-    intrinsic_goal_reward = 100
+    intrinsic_goal_reward = 200
     intrinsic_step_reward = -1
-    intrinsic_wrong_goal_reward = -10
+    intrinsic_wrong_goal_reward = -200
 
     # create and initialize the environment   
     env = Gridworld(n_dim, 
@@ -56,15 +57,15 @@ def init():
 
 
     # PARAMETERS OF MEATA CONTROLLER
-    meta_input_dim = 2 + n_dim**2
-    meta_nodes = [40, n_obj]
+    meta_input_dim = 2 + n_dim**2 + 1
+    meta_nodes = [40, 1]
     meta_layers = [Dense] * 2
     meta_inits = ['lecun_uniform'] * 2
-    meta_activations = ['relu'] * 1 + ['softmax']
+    meta_activations = ['relu'] * 2
     meta_loss = "mean_squared_error"
     meta_optimizer=RMSprop(lr=0.00025, rho=0.9, epsilon=1e-06)
     meta_batch_size = 1000
-    meta_epsilon = 1.0
+    meta_epsilon = 1
 
     # PARAMETERS OF THE CONTROLLER
     input_dim = 2 + 1 + n_dim**2
@@ -72,12 +73,12 @@ def init():
     nodes = [40, n_moves] 
     layers = [Dense] * 2
     inits = ['lecun_uniform'] * 2
-    activations = ['relu'] * 1 + ['softmax']
+    activations = ['relu'] * 2
     loss = "mean_squared_error"
     optimizer=RMSprop(lr=0.00025, rho=0.9, epsilon=1e-06)
     batch_size = 100
     gamma = 0.975
-    epsilon = 1.0
+    epsilon = 1
     tau = 0.001
 
     # create and initialize the agent
@@ -108,9 +109,9 @@ def init():
 def train_HRL(env, agent, num_thousands=12, num_epis=10):
 
     controllerExperience = namedtuple("controllerExperience", 
-        ["agent_state", "goal", "action", "reward", "next_agent_state", "done"])
+        ["state", "goal", "action", "reward", "next_state", "next_available_actions","controller_done"])
     MetaExperience = namedtuple("MetaExperience",   
-        ["agent_state", "goal", "reward", "next_agent_state", "done"])
+        ["state", "goal", "reward", "next_state", "next_available_goals", "done"])
 
     visits = np.zeros((num_thousands, env.n_dim, env.n_dim))
     anneal_factor = (1.0-0.1)/(num_thousands * num_epis)
@@ -121,65 +122,84 @@ def train_HRL(env, agent, num_thousands=12, num_epis=10):
     for episode_thousand in range(num_thousands):
         for episode in range(num_epis):
             print("\n\n\n\n### EPISODE "  + str(episode_thousand*num_thousands + episode) + "###")
-            agent_state = env.reset() # the returned agent_state is just a (2,) numpy array 
+            agent_state, env_state_flat = env.reset() # the returned agent_state is just a (2,) numpy array 
             visits[episode_thousand, agent_state[0,0], agent_state[0,1]] += 1
             terminal = False
             while not terminal:  # this loop is for meta-controller, while state not terminal
-                goal_idx, goal = agent.select_goal(agent_state)  # meta controller selects a goal
-                agent.goal_selected[goal_idx] += 1
+                goal = agent.select_goal(agent_state)  # meta controller selects a goal
+                # agent.goal_selected[goal_idx] += 1
                 print("\nNew Goal: "  + str(goal) + "\nState-Actions: ")
                 total_extrinsic_reward = 0
-                s0_agent = agent_state
+                s0 = state = np.concatenate((agent_state, env_state_flat), axis=1)
                 selected_goal_reached = False
                 while not terminal and not selected_goal_reached: # this loop is for meta, while state not terminal
-                    action_idx, action = agent.select_action(agent_state, goal, goal_idx) # controller selects an action among permitable actions
+                    action_idx, action = agent.select_action(agent_state, goal) # controller selects an action among permitable actions
                     # print(str((state,action)) + "; ")
-                    extrinsic_reward, next_agent_state = env.take_action(action_idx) # RIGHT NOW THE DONE IS NOT IMPLEMENTED YET 
-                    visits[episode_thousand, next_agent_state[0,0], next_agent_state[0,1]] += 1
+                    extrinsic_reward, next_agent_state, next_env_state_flat = env.take_action(action_idx) # RIGHT NOW THE DONE IS NOT IMPLEMENTED YET 
+                    i, j = next_agent_state[0,0], next_agent_state[0,1]
+                    visits[episode_thousand, i, j] += 1
                     intrinsic_reward, selected_goal_reached = env.intrinsic_critique(next_agent_state, goal)
 
-                    print ("action ----> "  + action)
-                    print ("state after action ----> " + "[" + str(next_agent_state[0,0]) +", " + 
-                            str(next_agent_state[0,1]) + "]" )
-                    print ("---------------------")
+                    # print ("action ----> "  + action)
+                    # print ("state after action ----> " + "[" + str(i) +", " + 
+                    #         str(j) + "]" )
+                    # print ("---------------------")
 
-                    terminal = env.is_terminal(next_agent_state)[0] or env.is_terminal(next_agent_state)[1]
-                    object_reached = env.grid_mat[next_agent_state[0,0], next_agent_state[0,1]]
+                    game_over = env.is_terminal(next_agent_state)[0]
+                    game_won = env.is_terminal(next_agent_state)[1]
+                    terminal = game_over or game_won
+                    object_reached = env.grid_mat[i,j]
                     if selected_goal_reached:
-                        agent.goal_success[goal_idx] += 1
+                        # agent.goal_success[goal_idx] += 1
                         print("SELECTED GOAL REACHED! ")
                         print("the object reached which should equal the selected goal: " + str(object_reached))
                         print ("original objects: {}".format(env.original_objects))
+                        if object_reached == env.current_target_goal:
+                            env.remove_occupied_object()
+                        print ("current objects: {}".format(env.current_objects))
                         print ("********************")
-                    if env.is_terminal(next_agent_state)[0]:
+
+                    if game_over:
                         game_over_counter += 1
-                        game_result_history.append("game over")
+                        game_result_history.append(0)
                         print("GAME OVER!!!") 
                         print("selected goal:" + str(goal))
                         print("the object reached: " +  str(object_reached))
                         print("the current target goal: " + str(env.current_target_goal))
-                        print ("original objects: {}".format(env.original_objects))                        
+                        print ("original objects: {}".format(env.original_objects))
+                        print ("current objects: {}".format(env.current_objects))                        
                         print("********************")
-                    if env.is_terminal(next_agent_state)[1]:
+                    if game_won:
                         game_won_counter += 1
-                        game_result_history.append("game over")
+                        game_result_history.append(1)
                         print("GAME WON!!!") 
                         print("the object reached: " +  str(object_reached))
                         print("the current target goal: " + str(env.current_target_goal))
-                        print ("original objects: {}".format(env.original_objects))                        
+                        print ("original objects: {}".format(env.original_objects)) 
+                        print ("current objects: {}".format(env.current_objects))                       
                         print("********************")
 
 
                     # if env.grid_mat[next_agent_state[0], next_agent_state[1]] == env.original_objects[-1]:
                     #     print("final object/number picked!! ")
-                    exp = controllerExperience(agent_state, goal, action_idx, intrinsic_reward, 
-                        next_agent_state, selected_goal_reached)
+                    controller_done = selected_goal_reached or terminal
+                    
+                    state = np.concatenate((env_state_flat, agent_state), axis=1)
+                    next_state = np.concatenate((next_env_state_flat, next_agent_state), axis=1)
+                    next_available_goals = env.current_objects
+                    next_available_actions = env.allowable_actions[i,j]
+
+                    # if the state is terminal, the experiment will not even be added to the controllerExperience
+                    # or the MetaEcperience, only the next_state can be terminal
+                    exp = controllerExperience(state, goal.reshape((1,1)), action_idx, intrinsic_reward,  
+                        next_state, next_available_actions, controller_done)
                     agent.store(exp, meta=False)
                     agent.update(meta=False)
                     agent.update(meta=True)
                     total_extrinsic_reward += extrinsic_reward
                     agent_state = next_agent_state
-                exp = MetaExperience(s0_agent, goal, total_extrinsic_reward, agent_state, terminal)
+                exp = MetaExperience(s0, goal.reshape((1,1)), total_extrinsic_reward, next_state, 
+                    next_available_goals, terminal)
                 agent.store(exp, meta=True)
                 # set_trace()
 
@@ -195,11 +215,14 @@ def train_HRL(env, agent, num_thousands=12, num_epis=10):
             
                 if(agent.epsilon < 0.1):
                     agent.epsilon = 0.1
+                if(agent.meta_epsilon) < 0.1:
+                    agent.epsilon = 0.1
 
 
             print("meta_epsilon: " + str(agent.meta_epsilon))
             print("epsilon: " + str(agent.epsilon))
 
+    print("SAVING THE LOG FILES .........")
     with open("logs/logs.txt", "w") as file:
         file.write("game_won_counter: {}\n".format(game_won_counter)) 
         file.write("game_over_counter: {}\n".format(game_over_counter))
@@ -207,16 +230,21 @@ def train_HRL(env, agent, num_thousands=12, num_epis=10):
     with open("logs/game_result_history.txt", "w") as file:
         for game in game_result_history:
             # item = game + "\n"
-            item = game + "\n"
+            item = str(game) + "\n"
             file.write(item)
 
+    with open("logs/game_result_history.pickle","wb") as file:
+        pickle.dump(game_result_history, file)
+
+
     print ("SAVING THE MODELS .............")  
+    print ()
     # serialize model to JSON
     model_json = agent.meta_controller.to_json()
     with open("saved_models/meta_controller.json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    agent.meta_controller.save_weights("meta_controller.h5")
+    agent.meta_controller.save_weights("saved_models/meta_controller.h5")
     print("Saved model to disk")
 
     # serialize model to JSON
@@ -224,7 +252,7 @@ def train_HRL(env, agent, num_thousands=12, num_epis=10):
     with open("saved_models/target_meta_controller.json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    agent.target_meta_controller.save_weights("target_meta_controller.h5")
+    agent.target_meta_controller.save_weights("saved_models/target_meta_controller.h5")
     print("Saved model to disk")
 
     # serialize model to JSON
@@ -232,15 +260,15 @@ def train_HRL(env, agent, num_thousands=12, num_epis=10):
     with open("saved_models/controller.json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    agent.controller.save_weights("controller.h5")
+    agent.controller.save_weights("saved_models/controller.h5")
     print("Saved model to disk")
 
-    # serialize model to JSON
+  
     model_json = agent.target_controller.to_json()
     with open("saved_models/target_controller.json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    agent.meta_controller.save_weights("target_controller.h5")
+    agent.meta_controller.save_weights("saved_models/target_controller.h5")
     print("Saved model to disk")
 
 
@@ -257,13 +285,13 @@ def train_controller(env, agent):
 
         terminal = False
         while not terminal:  # this loop is for meta-controller, while state not terminal
-            goal_idx, goal = agent.select_goal(agent_state)  # meta controller selects a goal
-            agent.goal_selected[goal_idx] += 1
+            goal = agent.select_goal(agent_state)  # meta controller selects a goal
+            # agent.goal_selected[goal_idx] += 1
             print("\nNew Goal: "  + str(goal) + "\nState-Actions: ")
             s0_agent = agent_state
             selected_goal_reached = False
             while not terminal and not selected_goal_reached:
-                action_idx, action = agent.select_action(agent_state, goal, goal_idx) # controller selects an action among permitable actions
+                action_idx, action = agent.select_action(agent_state, goal) # controller selects an action among permitable actions
                 # print(str((state,action)) + "; ")
                 extrinsic_reward, next_agent_state = env.take_action(action_idx) # RIGHT NOW THE DONE IS NOT IMPLEMENTED YET 
                 intrinsic_reward, selected_goal_reached = env.intrinsic_critique(next_agent_state, goal)
@@ -277,7 +305,7 @@ def train_controller(env, agent):
                 terminal = env.is_terminal(next_agent_state)[0] or env.is_terminal(next_agent_state)[1]
                 object_reached = env.grid_mat[next_agent_state[0,0], next_agent_state[0,1]]
                 if selected_goal_reached:
-                    agent.goal_success[goal_idx] += 1
+                    # agent.goal_success[goal_idx] += 1
                     print("SELECTED GOAL REACHED! ")
                     print("the object reached which should equal the selected goal: " + str(object_reached))
                     print ("original objects: {}".format(env.original_objects))
@@ -304,7 +332,7 @@ def train_controller(env, agent):
         
 
         #Annealing, just anneal the controller epsilon
-        avg_success_rate = agent.goal_success[goal_idx] / agent.goal_selected[goal_idx]
+        # avg_success_rate = agent.goal_success[goal_idx] / agent.goal_selected[goal_idx]
         annealgent.epsilon -= anneal_factor
         
         # if(avg_success_rate == 0 or avg_success_rate == 1):
